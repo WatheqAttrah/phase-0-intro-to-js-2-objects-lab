276eff0e5b3ced11928a137b4eccfee5b3ff85ba
Watheq Attrah <novaatrah@gmail.com>
2023-03-12 17:01:53 -0700

    Complete

--- test/indexTest.js
+++ test/indexTest.js
@@ -9,7 +9,7 @@ describe('employees', function() {
 
       employee.name = 'Sam';
     });
-
+ 
     it('returns an employee with the original key value pairs and the new key value pair', function () {
       expect(updateEmployeeWithKeyAndValue(employee, 'streetAddress', '11 Broadway')).to.eql({
         name: 'Sam',
